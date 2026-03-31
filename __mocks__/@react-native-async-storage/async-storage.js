f7d3659ce1f47f061ba54687e7804eca25928db1
zeinajah <zeinnugroho167@gmail.com>
2026-03-31 08:15:14 +0700

    ready?

--- __mocks__/@react-native-async-storage/async-storage.js
+++
@@ -1,8 +0,0 @@
-// __mocks__/@react-native-async-storage/async-storage.js
-module.exports = {
-  getItem: jest.fn(),
-  setItem: jest.fn(),
-  removeItem: jest.fn(),
-  multiRemove: jest.fn(),
-  clear: jest.fn(),
-};
